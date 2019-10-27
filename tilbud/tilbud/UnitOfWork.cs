c593609cc745709fc44fceab672520611d848625
martinsekd <201710685@post.au.dk>
2019-10-27 17:43:35 +0100

    added Unit of work

---
+++ tilbud/tilbud/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tilbud
+{
+    public class UnitOfWork : IDisposable
+    {
+        private prj4databaseContext context = new prj4databaseContext();
+        //private Repository<Vare> vareRepository;
+
+        public void Save() { context.SaveChanges(); }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
