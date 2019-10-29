b118cd4e7fd92cdaf8ab6ad0562156aa643e7fab
Jogans <rasmusdv@hotmail.com>
2019-10-29 11:46:38 +0100

    Tilføjet første repository klasse for vare

---
+++ tilbud/Testprogram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using tilbud;
+using Transfer.Models;
+
+namespace Testprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            prj4databaseContext context = new prj4databaseContext();
+            VareRepository objekt = new VareRepository(context);
+
+            Vare var1 = new Vare();
+            //var1.VareId = 50000;
+            var1.Navn = "Testvare";
+            var1.Pris = 2000.0;
+            var1.Butik = "Netto";
+            var1.ValidFra = "";
+            var1.ValidTil = "";
+            var1.Volumenpris = 100;
+            var1.Volumen = 200;
+            var1.Imgsrc = "";
+            
+            objekt.InsertVare(var1);
+            objekt.Save();
+        }
+    }
+}
