933a9f6a799b8ab374b3a9a2154bd7dbebf5b783
martinsekd <201710685@post.au.dk>
2019-11-02 11:37:24 +0100

    updated EF Core with database

--- tilbud/tilbud/Models/UnitOfWork.cs
+++ tilbud/tilbud/Models/UnitOfWork.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace tilbud
 {
@@ -16,4 +17,5 @@ namespace tilbud
             throw new NotImplementedException();
         }
     }
+
 }
