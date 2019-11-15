2df8ae7910fd9c46ec1c08b20ae81aaaa6a84d24
Jogans <201404212@post.au.dk>
2019-11-15 14:06:20 +0100

    TIlføjet første repositories og en query til recipe

---
+++ Guldtand MVC m. Identity/GuldtandMVC_Identity/Data/Queries/RecipeQuery.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace GuldtandMVC_Identity.Data.Queries
+{
+    public class RecipeQuery
+    {
+        public bool LoadIngredientList { get; set; } = false;
+        public bool LoadRecipeCategory { get; set; } = false;
+        public string SearchRecipe { get; set; } = "";
+
+        public async Task<IEnumerable<Recipe>> Execute(prj4databaseContext context)
+        {
+            switch (LoadIngredientList)
+            {
+                case true:
+                    switch (LoadRecipeCategory)
+                    {
+                        case true:
+                            return await context.Set<Recipe>()
+                                .Where(r => r.Name.Contains(SearchRecipe))
+                                .Include(r => r.IngredientList)
+                                .Include(r => r.RecipeCategory)
+                                .ToListAsync();
+                        case false:
+                            return await context.Set<Recipe>()
+                                .Where(r => r.Name.Contains(SearchRecipe))
+                                .Include(r => r.IngredientList)
+                                .ToListAsync();
+                        default:
+                            return await context.Set<Recipe>()
+                                .Where(r => r.Name.Contains(SearchRecipe))
+                                .ToListAsync();
+                    }
+                    break;
+                case false:
+                    switch (LoadRecipeCategory)
+                    {
+                        case true:
+                            return await context.Set<Recipe>()
+                                .Where(r => r.Name.Contains(SearchRecipe))
+                                .Include(r => r.RecipeCategory)
+                                .ToListAsync();
+                        case false:
+                            return await context.Set<Recipe>()
+                                .Where(r => r.Name.Contains(SearchRecipe))
+                                .ToListAsync();
+                        default:
+                            return await context.Set<Recipe>()
+                                .Where(r => r.Name.Contains(SearchRecipe))
+                                .ToListAsync();
+                    }
+                default:
+                    return await context.Set<Recipe>()
+                        .Where(r => r.Name.Contains(SearchRecipe))
+                        .ToListAsync();
+            }
+        }
+    }
+}
---
+++ Guldtand MVC m. Identity/GuldtandMVC_Identity/Data/Repositories/RecipeRepository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace GuldtandMVC_Identity.Data.Repositories
+{
+    public class RecipeRepository : IRecipeRepository, IDisposable
+    {
+        private prj4databaseContext context;
+
+        public RecipeRepository(prj4databaseContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Recipe> GetRecipes()
+        {
+            return context.Recipe.ToList();
+        }
+
+        public void InsertRecipe(Recipe recipe)
+        {
+            context.Recipe.Add(recipe);
+        }
+
+        public void DeleteRecipe(int recipeId)
+        {
+            Recipe recipe = context.Recipe.Find(recipeId);
+            context.Recipe.Remove(recipe);
+        }
+
+        public void UpdateRecipe(Recipe recipe)
+        {
+            context.Entry(recipe).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
